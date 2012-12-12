02bd40850c9a659d79292c5accf8e6a8636b27ee
Miriam <miriam@miriam-LIFEBOOK-E8010.(none)>
2012-12-12 21:55:27 +0100

    initial commit

---
+++ src/de/bennir/DVBViewerController/activity_main.java
@@ -0,0 +1,15 @@
+package de.bennir.DVBViewerController;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_main extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
