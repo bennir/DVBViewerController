2b7589fe33a30bf39147c1fd0d2748686ec32eb7
Benni R <benni.rueder@gmail.com>
2013-04-11 11:30:54 +0200

    Fixed issue #1

--- src/de/bennir/DVBViewerController/RemoteFragment.java
+++ src/de/bennir/DVBViewerController/RemoteFragment.java
@@ -1,9 +1,12 @@
 package de.bennir.DVBViewerController;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
 import android.view.*;
 import android.widget.ImageView;
 import com.actionbarsherlock.app.SherlockFragment;
@@ -40,109 +43,83 @@ public class RemoteFragment extends SherlockFragment {
         remote.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                int coords[] = new int[2];
-                v.getLocationOnScreen(coords);
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    int coords[] = new int[2];
+                    v.getLocationOnScreen(coords);
 
-                int x = (int) event.getRawX() - coords[0];
-                int y = (int) event.getRawY() - coords[1];
+                    int x = (int) event.getRawX() - coords[0];
+                    int y = (int) event.getRawY() - coords[1];
 
-                ImageView img = (ImageView) getActivity().findViewById(
-                        R.id.remote_touchmap);
-                Bitmap bitmap = ((BitmapDrawable) img.getDrawable())
-                        .getBitmap();
-                int pixel = bitmap.getPixel(x, y);
+                    ImageView img = (ImageView) getActivity().findViewById(
+                            R.id.remote_touchmap);
+                    Bitmap bitmap = ((BitmapDrawable) img.getDrawable())
+                            .getBitmap();
+                    int pixel = bitmap.getPixel(x, y);
 
-                int red = Color.red(pixel);
-                int green = Color.green(pixel);
-                int blue = Color.blue(pixel);
+                    int red = Color.red(pixel);
+                    int green = Color.green(pixel);
+                    int blue = Color.blue(pixel);
 
-                /**
-                 * Button Events
-                 */
+                    /**
+                     * Button Events
+                     */
 
-                // Chan+
-                if (red == 119 && blue == 119 && green == 119) {
-                    // Log.e("Event: ", "Chan+");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendUp");
-                }
-                // Chan-
-                if (red == 0 && blue == 0 && green == 0) {
-                    // Log.e("Event: ", "Chan-");
-                    sendCommand("sendDown");
-                }
-                // Vol+
-                if (red == 49 && blue == 49 && green == 49) {
-                    // Log.e("Event: ", "Vol+");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendRight");
-                }
-                // Vol-
-                if (red == 204 && blue == 204 && green == 204) {
-                    // Log.e("Event: ", "Vol-");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendLeft");
-                }
-                // Menu
-                if (red == 0 && blue == 255 && green == 255) {
-                    // Log.e("Event: ", "Menu");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendMenu");
-                }
-                // Ok
-                if (red == 255 && blue == 255 && green == 0) {
-                    // Log.e("Event: ", "Ok");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendOk");
-                }
-                // Back
-                if (red == 255 && blue == 0 && green == 168) {
-                    // Log.e("Event: ", "Back");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendBack");
-                }
-                // Red
-                if (red == 255 && blue == 0 && green == 0) {
-                    // Log.e("Event: ", "Red");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendRed");
-                }
-                // Yellow
-                if (red == 255 && blue == 0 && green == 255) {
-                    // Log.e("Event: ", "Yellow");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendYellow");
-                }
-                // Green
-                if (red == 0 && blue == 0 && green == 255) {
-                    // Log.e("Event: ", "Green");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendGreen");
-                }
-                // Blue
-                if (red == 0 && blue == 255 && green == 0) {
-                    // Log.e("Event: ", "Blue");
-                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-                    sendCommand("sendBlue");
+                    // Chan+
+                    if (red == 119 && blue == 119 && green == 119) {
+                        sendCommand("sendUp");
+                    }
+                    // Chan-
+                    if (red == 0 && blue == 0 && green == 0) {
+                        sendCommand("sendDown");
+                    }
+                    // Vol+
+                    if (red == 49 && blue == 49 && green == 49) {
+                        sendCommand("sendRight");
+                    }
+                    // Vol-
+                    if (red == 204 && blue == 204 && green == 204) {
+                        sendCommand("sendLeft");
+                    }
+                    // Menu
+                    if (red == 0 && blue == 255 && green == 255) {
+                        sendCommand("sendMenu");
+                    }
+                    // Ok
+                    if (red == 255 && blue == 255 && green == 0) {
+                        sendCommand("sendOk");
+                    }
+                    // Back
+                    if (red == 255 && blue == 0 && green == 168) {
+                        sendCommand("sendBack");
+                    }
+                    // Red
+                    if (red == 255 && blue == 0 && green == 0) {
+                        sendCommand("sendRed");
+                    }
+                    // Yellow
+                    if (red == 255 && blue == 0 && green == 255) {
+                        sendCommand("sendYellow");
+                    }
+                    // Green
+                    if (red == 0 && blue == 0 && green == 255) {
+                        sendCommand("sendGreen");
+                    }
+                    // Blue
+                    if (red == 0 && blue == 255 && green == 0) {
+                        sendCommand("sendBlue");
+                    }
                 }
 
-                return false;
+                return true;
             }
         });
 
     }
 
     public void sendCommand(String command) {
+        Log.d(TAG, "Remote Command: " + command);
+
+        ((Vibrator) getSherlockActivity().getSystemService(Context.VIBRATOR_SERVICE)).vibrate(50);
         if (DVBViewerControllerActivity.dvbHost != "Demo Device") {
             String url = "http://" +
                     DVBViewerControllerActivity.dvbIp + ":" +
